87b66d4883a3c19631f3c9a2fe1b8ae2301f3f90
Yash Jangid <yashjangid13062004@gmail.com>
2025-07-01 11:52:09 +0530

    BFS and DFS in one file

--- graphs/BFS_DFS.cpp
+++ graphs/BFS_DFS.cpp
@@ -24,11 +24,24 @@ vector<int> BFS (int start , int n , vector<vector<int>> &adj ) {
     return result ; 
 }
 
+void DFS ( int start , vector<vector<int>> & adj , vector<bool> & visited ){
+    visited[start] = true ; 
+    cout << start << " " ; 
+    for ( auto neighbor : adj[start] ) if ( !visited[neighbor] ) DFS( neighbor , adj , visited ) ; 
+}
+
 int main()
 {
     vector<vector<int>> adj = {{1,2}, {0,2,3}, {0,4}, {1,4}, {2,3}};
     vector<int> result = BFS(0 , adj.size() , adj ) ; 
 
+    vector<bool> visited ( adj.size() , false ) ; 
+
+    cout << "by DFS traversal " << endl; 
+    DFS (0 , adj , visited) ; 
+
+
+    cout << " by BFS traversal " << endl ; 
     for ( auto x : result ){
         cout << x << " " ; 
     }
